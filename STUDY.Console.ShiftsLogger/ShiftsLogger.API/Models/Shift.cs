fefd4de2dd1384ff6dbf691ff4197e2113a90fb8
basemkasem <69323267+basemkasem@users.noreply.github.com>
2025-08-24 04:08:48 +0300

    Create models, database migration and shift service

--- STUDY.Console.ShiftsLogger/ShiftsLogger.API/Models/Shift.cs
+++ STUDY.Console.ShiftsLogger/ShiftsLogger.API/Models/Shift.cs
@@ -2,5 +2,9 @@
 
 public class Shift
 {
-
+    public int Id { get; set; }
+    public DateTimeOffset StartTime { get; set; }
+    public DateTimeOffset EndTime { get; set; }
+    public int WorkerId { get; set; }
+    public Worker? Worker { get; set; }
 }
