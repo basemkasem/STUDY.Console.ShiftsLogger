bc887be55361ae0e5ff2a2d35dbca396485f76b3
basem <basem.kasem16@gmail.com>
2026-02-19 14:28:27 +0200

    add try-catch blocks for shift operations

--- STUDY.Console.ShiftsLogger/ShiftsLogger.UI/Controller/ShiftsController.cs
+++ STUDY.Console.ShiftsLogger/ShiftsLogger.UI/Controller/ShiftsController.cs
@@ -10,122 +10,144 @@ public class ShiftsController
 
     public async Task AddShift(WorkerDto workerDto)
     {
-        bool isValid = false;
-        DateTime startTime = default;
-        DateTime endTime = default;
-        while (!isValid)
+        try
         {
-            //TODO: Show valid Formats for user
-            startTime = AnsiConsole.Ask<DateTime>("Enter start time: ");
-            endTime = AnsiConsole.Ask<DateTime>("Enter end time: ");
-            if (endTime <= startTime)
+            bool isValid = false;
+            DateTime startTime = default;
+            DateTime endTime = default;
+            while (!isValid)
             {
-                AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
+                //TODO: Show valid Formats for user
+                startTime = AnsiConsole.Ask<DateTime>("Enter start time: ");
+                endTime = AnsiConsole.Ask<DateTime>("Enter end time: ");
+                if (endTime <= startTime)
+                {
+                    AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
+                }
+                else
+                {
+                    isValid = true;
+                }
             }
-            else
-            {
-                isValid = true;
-            }
-        }
 
-        ShiftCreationDto shiftCreationDto = new()
-        {
-            StartTime = startTime,
-            EndTime = endTime,
-            WorkerId = workerDto.Id
-        };
-        var shiftIsCreated = await _shiftService.AddShift(shiftCreationDto);
-        if (shiftIsCreated)
-        {
-            AnsiConsole.MarkupLine("[green]Shift created successfully![/]");
+            ShiftCreationDto shiftCreationDto = new()
+            {
+                StartTime = startTime,
+                EndTime = endTime,
+                WorkerId = workerDto.Id
+            };
+            var shiftIsCreated = await _shiftService.AddShift(shiftCreationDto);
+            AnsiConsole.MarkupLine(shiftIsCreated
+                ? "[green]Shift created successfully![/]"
+                : "[red]Shift not created![/]");
         }
-        else
+        catch (Exception e)
         {
-            AnsiConsole.MarkupLine("[red]Shift not created![/]");
+            AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+            AnsiConsole.MarkupLine($"[red]Shift not created![/]");
         }
     }
 
     public async Task UpdateShift(WorkerDto workerDto)
     {
-        var workerShifts = await _shiftService.GetShiftsByWorkerId(workerDto.Id);
-        if (workerShifts is null)
+        try
         {
-            return;
-        }
-
-        var shiftChoice = AnsiConsole.Prompt(
-            new SelectionPrompt<ShiftDto>()
-                .Title("Choose a Shift to Update:")
-                .AddChoices(workerShifts)
-        );
-        var updateOption = AnsiConsole.Prompt<string>(
-            new SelectionPrompt<string>()
-                .Title($"Update for shift: {shiftChoice}")
-                .AddChoices("Update Start Time", "Update End Time", "Update Start Time and End Time", "Cancel")
-        );
-        DateTime startTime;
-        DateTime endTime;
-        switch (updateOption)
-        {
-            case "Update Start Time":
-                startTime = AnsiConsole.Ask<DateTime>("Enter new start time: ");
-                while (startTime >= shiftChoice.EndTime)
-                {
-                    AnsiConsole.MarkupLine("[Red]Start time is not valid. Please try again:[/]");
-                    startTime = AnsiConsole.Ask<DateTime>("Enter new start time: ");
-                }
-                shiftChoice.StartTime = startTime;
-                break;
-            case "Update End Time":
-                endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
-                while (endTime <= shiftChoice.StartTime)
-                {
-                    AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
-                    endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
-                }
-
-                shiftChoice.EndTime = endTime;
-                break;
-            case "Update Start Time and End Time":
-                 startTime = AnsiConsole.Ask<DateTime>("Enter new start time: ");
-                 endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
-                 while (endTime <= startTime)
-                 {
-                     AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
-                     endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
-                 }
-                 shiftChoice.StartTime = startTime;
-                 shiftChoice.EndTime = endTime;
-                break;
-            case "Cancel":
+            var workerShifts = await _shiftService.GetShiftsByWorkerId(workerDto.Id);
+            if (workerShifts is null)
+            {
                 return;
-        }
+            }
 
-        var shiftIsUpdated = await _shiftService.UpdateShift(shiftChoice);
-        if(shiftIsUpdated)
-            AnsiConsole.MarkupLine("[green]Shift Updated Successfully![/]");
-        else
-            AnsiConsole.MarkupLine("[red]Shift is NOT Updated[/]");
+            var shiftChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<ShiftDto>()
+                    .Title("Choose a Shift to Update:")
+                    .AddChoices(workerShifts)
+            );
+            var updateOption = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title($"Update for shift: {shiftChoice}")
+                    .AddChoices("Update Start Time", "Update End Time", "Update Start Time and End Time", "Cancel")
+            );
+            DateTime startTime;
+            DateTime endTime;
+            switch (updateOption)
+            {
+                case "Update Start Time":
+                    startTime = AnsiConsole.Ask("Enter new start time - or press ENTER to keep old record: ",
+                        shiftChoice.StartTime);
+                    while (startTime >= shiftChoice.EndTime)
+                    {
+                        AnsiConsole.MarkupLine("[Red]Start time is not valid. Please try again:[/]");
+                        startTime = AnsiConsole.Ask(
+                            "Enter new start time- or press ENTER to keep old record: ", shiftChoice.StartTime);
+                    }
+
+                    shiftChoice.StartTime = startTime;
+                    break;
+                case "Update End Time":
+                    endTime = AnsiConsole.Ask("Enter new end time - or press ENTER to keep old record: ",
+                        shiftChoice.EndTime);
+                    while (endTime <= shiftChoice.StartTime)
+                    {
+                        AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
+                        endTime = AnsiConsole.Ask("Enter new end time- or press ENTER to keep old record: ",
+                            shiftChoice.EndTime);
+                    }
+
+                    shiftChoice.EndTime = endTime;
+                    break;
+                case "Update Start Time and End Time":
+                    startTime = AnsiConsole.Ask<DateTime>("Enter new start time: ");
+                    endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
+                    while (endTime <= startTime)
+                    {
+                        AnsiConsole.MarkupLine("[Red]End time is not valid. Please try again:[/]");
+                        endTime = AnsiConsole.Ask<DateTime>("Enter new end time: ");
+                    }
+
+                    shiftChoice.StartTime = startTime;
+                    shiftChoice.EndTime = endTime;
+                    break;
+                case "Cancel":
+                    return;
+            }
+
+            var shiftIsUpdated = await _shiftService.UpdateShift(shiftChoice);
+            AnsiConsole.MarkupLine(shiftIsUpdated
+                ? "[green]Shift Updated Successfully![/]"
+                : "[red]Shift is NOT Updated[/]");
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+            AnsiConsole.MarkupLine($"[red]Shift is NOT Updated[/]");
+        }
     }
 
     public async Task DeleteShift(WorkerDto workerDto)
     {
-        var workerShifts = await _shiftService.GetShiftsByWorkerId(workerDto.Id);
-        if (workerShifts is null)
+        try
         {
-            return;
-        }
+            var workerShifts = await _shiftService.GetShiftsByWorkerId(workerDto.Id);
+            if (workerShifts is null)
+            {
+                return;
+            }
 
-        var shiftChoice = AnsiConsole.Prompt(
-            new SelectionPrompt<ShiftDto>()
-                .Title("Choose a Shift to Delete:")
-                .AddChoices(workerShifts)
-        );
-        
-        var shiftIsDeleted = await _shiftService.DeleteShift(shiftChoice.Id);
-        if (shiftIsDeleted)
-            AnsiConsole.MarkupLine("[green]Shift Deleted Successfully![/]");
-        else
-            AnsiConsole.MarkupLine("[red]Shift is NOT Deleted[/]");
+            var shiftChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<ShiftDto>()
+                    .Title("Choose a Shift to Delete:")
+                    .AddChoices(workerShifts)
+            );
+
+            var shiftIsDeleted = await _shiftService.DeleteShift(shiftChoice.Id);
+            AnsiConsole.MarkupLine(
+                shiftIsDeleted ? "[green]Shift Deleted Successfully![/]" : "[red]Shift is NOT Deleted[/]");
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.MarkupLine($"[red]{e.Message}[/]");
+            AnsiConsole.MarkupLine($"[red]Shift is NOT Deleted[/]");
+        }
     }
 }
